74f952eb1fea7a840d095d51ce589bf50e60f1e6
azertypow <azertypow@gmail.com>
2022-07-08 17:58:21 +0200

    chore: test css url panel

--- site/config/config.php
+++ site/config/config.php
@@ -3,7 +3,7 @@
 return [
     'debug' => true,
     'panel' => [
-        'css' => '/home/clients/d98898ecb865f1c357dfc40169febc89/sites/api.les6toits.ch/kirby/config/panel.css'
+        'css' => 'assets/css/custom-panel.css'
     ],
     'routes' => [
         [
