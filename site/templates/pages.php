bbd27790850ac26860f02ff31d9fd8e865c5f8f2
azertypow <azertypow@gmail.com>
2023-02-19 22:05:22 +0100

    fix: api pages retour all pages

--- site/templates/pages.php
+++ site/templates/pages.php
@@ -19,7 +19,7 @@ echo json_encode( $page->children()->map(function ($value) {
       return [
         'text' => $structureValue->text()->value(),
         'image' => $structureValue->image()->toFile() ? imageApiResponse( $structureValue->image()->toFile() ) : null,
-        'allFiles' => $structureValue->image()->toFile(),
+        'allFiles' => $structureValue->image()->toFile()->toArray(),
       ];
     }) -> data()]
   );
