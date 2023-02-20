7a46ba83b8c3d58a5b8b6aa1b01c22bb36499b29
azertypow <azertypow@gmail.com>
2023-02-20 11:00:45 +0100

    feat: remove Git push action

--- transfert.sh
+++ transfert.sh
@@ -1,4 +1,4 @@
-git push
+#git push
 ssh om1cf_nico0033671139446@om1cf.ftp.infomaniak.com /bin/bash << EOF
 cd /home/clients/d98898ecb865f1c357dfc40169febc89/sites/api.les6toits.ch
 echo 'init git clone in directory' && pwd
