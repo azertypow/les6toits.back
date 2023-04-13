976e845285d64d0103ec08a82e01063f7e86f4a6
azertypow <azertypow@gmail.com>
2023-04-13 12:34:23 +0200

    fix: doc

--- transfert.sh
+++ transfert.sh
@@ -1,6 +1,6 @@
 #git push
 ssh om1cf_nico0033671139446@om1cf.ftp.infomaniak.com /bin/bash << EOF
 cd /home/clients/d98898ecb865f1c357dfc40169febc89/sites/api.les6toits.ch
-echo 'init git clone in directory' && pwd
+echo 'init git pull in directory' && pwd
 git pull
 EOF
