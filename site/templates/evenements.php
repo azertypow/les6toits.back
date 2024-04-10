4f1cb1708fdb5f26cab6e154eec83cd0810106a7
azertypow <azertypow@gmail.com>
2024-04-10 13:28:19 +0200

    feat: add archive api response

--- site/templates/evenements.php
+++ site/templates/evenements.php
@@ -22,5 +22,22 @@ echo json_encode([
     'credit'        => $listedPage -> credit()->value(),
     'location'      => $listedPage -> location()->value(),
 
-  ])->data())
+  ])->data()),
+
+  'archives' => array_values($page->children()->unlisted()->map(fn($listedPage) => [
+    'dates'         => $listedPage->content()->dates()->toStructure()->map(fn($date) => [
+      'jours'           => $date->date()->value(),
+      'hours'           => array_map(fn($hour) => $hour['heure'],$date->heures()->value())
+    ])->data(),
+
+    'tags'          => $listedPage->content()->tags()->value(),
+    'imageCoverURL' => imageApiResponse( $listedPage->imageCoverURL()->toFile() ),
+    'title'         => $listedPage -> title()->value(),
+    'subtitle'      => $listedPage -> subtitle() -> value(),
+    'linkCoverURL'  => $listedPage -> linkCoverURL() -> value(),
+    'textContent'   => $listedPage -> textContent()->value(),
+    'credit'        => $listedPage -> credit()->value(),
+    'location'      => $listedPage -> location()->value(),
+
+  ])->data()),
 ]);
